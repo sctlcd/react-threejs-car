71832f74328869cfad72b8298cb229a64ff57898
lucinda <lucinda.souchet@gmail.com>
2023-01-23 15:13:52 +0100

    remove src files, update structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"; 
+// package that provides client-specific methods used for initializing an app on the client
+import { createRoot } from "react-dom/client";
+// React renderer for three.js
+import { Canvas } from "@react-three/fiber";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// create a React root for displaying content inside a browser DOM element
+createRoot(document.getElementById("root")).render(
+    // The Canvas object is your portal into Threejs. It renders Threejs elements, not DOM elements!
+    // The Canvas object is where you start to define your React Three Fiber Scene.
+    // All three.js scripts should be wrap under the canvas component 
+    // since we can't use @react-three/fiber hooks outside of canvas (Scene component, etc.)
+    <Canvas></Canvas>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
