642e7ed3f6092c1a930bbc0c8c7a82006e9191b1
sctlcd <lucinda.souchet@gmail.com>
2023-10-19 17:08:20 +0200

    add 2 3D model cars and set position on the canvas

--- src/Car.jsx
+++ src/Car.jsx
@@ -3,18 +3,27 @@ import { useEffect } from "react";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export function Car() {
-  const meshModelPath = `${process.env.PUBLIC_URL}/models/`;
-  const meshModelName  = "car.glb";
-  const meshModelFile = meshModelPath + meshModelName;
+  const modelsFolderPath = `${process.env.PUBLIC_URL}/models/`;
+  const modelFileName  = "peugeot_205_gti.glb";
+  const modelFilePath = `${modelsFolderPath}${modelFileName}`;
 
   const mesh = useLoader(
     GLTFLoader,
-    meshModelFile,
+    modelFilePath,
   ).scene;
 
   useEffect(() => {
-    mesh.scale.set(0.0012, 0.0012, 0.0012);
-    mesh.children[0].position.set(-365, -18, -67);
+    // car.glb
+    // mesh.scale.set(0.0012, 0.0012, 0.0012); 
+    // mesh.children[0].position.set(-365, -18, -67);
+    
+    // 65_chevy_malibu.glb
+    // mesh.scale.set(0.08, 0.08, 0.08); 
+    // mesh.children[0].position.set( 10, 0, 0);
+    
+    // peugeot_205_gti.glb
+    mesh.scale.set(0.2, 0.2, 0.2); 
+    mesh.children[0].position.set( 1, 0.4, 0);
   }, [mesh]);
 
   return (
